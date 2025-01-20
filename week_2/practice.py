c6f587998a9c38d5a0a450ad34daf87148f74d10
Segeorge0518 <segeorge0518@sbcglobal.net>
2025-01-19 21:48:11 -0500

    Week 2 assignment commit

---
+++ week_2/practice.py
@@ -0,0 +1,18 @@
+'''
+Author: Sara George
+Date: 1/18/2025
+Filename: practice.py
+Description: WEB335 Exercise 2.3/Hands-On 1.1. First Python Program  
+'''
+
+
+
+
+#A first pass at printing a straight quote & attribution to the console
+print('"Never think that war, no matter how necessary nor how justified, is not a crime. Ask the infantry and ask the dead." - Ernest Hemingway')
+
+#The following are variables and a console print command that are used to build and print a concatenated statement to the console 
+quote_author = 'Ernest Hemingway'
+quote = 'Never think that war, no matter how necessary nor how justified, is not a crime. Ask the infantry and ask the dead.'
+
+print('One of my favorite quotes is by ' + quote_author + ': "' + quote + '"')
